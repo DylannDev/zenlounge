7dc88c5fc5f2265f57adeda0dd506f6ada3d6cca
dylann97 <d.xavero@hotmail.com>
2024-12-17 01:13:41 +0100

    Adjust aspect ratio of Hero Image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,31 +4,32 @@ import Button from "./Button";
 const Hero = () => {
   return (
     <div className="">
-      <div className="relative w-full h-[900px] lg:h-[800px]">
+      <div className="relative w-full aspect-1/2 min-[500px]:aspect-2/3 sm:aspect-3/4 md:aspect-4/3 lg:aspect-video max-h-[calc(100dvh-140px)]">
         <Image
           src={"/massage-1.jpg"}
-          alt="bannière bijoux oshun jewelry"
+          alt="massage photo"
           fill
-          sizes="100%"
-          className="object-cover object-bottom md:object-top brightness-95 rounded-3xl"
+          sizes="100vw"
+          quality={100}
+          className="object-cover object-bottom md:object-center brightness-95 rounded-3xl"
           priority
         />
 
         <div className="relative w-full h-full grid place-content-center">
           <div className="absolute bottom-0 flex flex-col justify-start gap-4 p-6 md:p-12 w-full lg:w-2/3 xl:w-1/2 text-brown-dark">
             <h1 className="font-bold text-4xl md:text-5xl">
-              Offrez-vous un moment de détente et bien-être
+              Offrez-vous un moment de détente
             </h1>
             <p className="font-medium text-lg">
               Massages, soins du corps et visage, et séjours détente en Guyane.
             </p>
             <div className="flex md:hidden">
-              <Button color="empty" width="large">
+              <Button color="white" width="large">
                 Réserver mon moment
               </Button>
             </div>
             <div className="hidden md:flex">
-              <Button color="empty" width="normal">
+              <Button color="white" width="normal">
                 Réserver mon moment
               </Button>
             </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,85 +2,88 @@ import type { Config } from "tailwindcss";
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			brown: {
-  				light: '#944524',
-  				DEFAULT: '#73361c',
-  				dark: '#522714',
-  				background: '#171310'
-  			},
-  			rose: {
-  				background: '#FEF4EC',
-  				light: '#FDE9D8',
-  				DEFAULT: '#FCDFC5',
-  				dark: '#fbd3b1'
-  			},
-  			blue: {
-  				light: '#3D4852',
-  				DEFAULT: '#242b31',
-  				dark: '#111517'
-  			}
-  		},
-  		fontFamily: {
-  			satoshi: [
-  				'Satoshi',
-                    ...fontFamily.sans
-                ]
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			},
-  			marquee: {
-  				from: {
-  					transform: 'translateX(0)'
-  				},
-  				to: {
-  					transform: 'translateX(calc(-100% - var(--gap)))'
-  				}
-  			},
-  			'marquee-vertical': {
-  				from: {
-  					transform: 'translateY(0)'
-  				},
-  				to: {
-  					transform: 'translateY(calc(-100% - var(--gap)))'
-  				}
-  			}
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
-  			marquee: 'marquee var(--duration) infinite linear',
-  			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite'
-  		}
-  	}
+    extend: {
+      colors: {
+        brown: {
+          light: "#944524",
+          DEFAULT: "#73361c",
+          dark: "#522714",
+          background: "#171310",
+        },
+        rose: {
+          background: "#FEF4EC",
+          light: "#FDE9D8",
+          DEFAULT: "#FCDFC5",
+          dark: "#fbd3b1",
+        },
+        blue: {
+          light: "#3D4852",
+          DEFAULT: "#242b31",
+          dark: "#111517",
+        },
+      },
+      fontFamily: {
+        satoshi: ["Satoshi", ...fontFamily.sans],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      aspectRatio: {
+        "1/2": "1 / 2",
+        "2/3": "2 / 3",
+        "3/4": "3 / 4",
+        "4/3": "4 / 3",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+        marquee: {
+          from: {
+            transform: "translateX(0)",
+          },
+          to: {
+            transform: "translateX(calc(-100% - var(--gap)))",
+          },
+        },
+        "marquee-vertical": {
+          from: {
+            transform: "translateY(0)",
+          },
+          to: {
+            transform: "translateY(calc(-100% - var(--gap)))",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        marquee: "marquee var(--duration) infinite linear",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
