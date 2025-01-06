c94b5b4cffc119410e4e73aa6f5da98cf4e32163
dylann97 <d.xavero@hotmail.com>
2025-01-06 23:42:13 +0100

    ♻️ Delete and add utils function in utils.ts to avoid double folders

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,39 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const capitalizeFirstLetter = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const timeToMinutes = (time: string): number => {
+  const [hours, minutes] = time.split(":").map(Number);
+  return hours * 60 + minutes;
+};
+
+// console.log(timeToMinutes("09:30")); // 570
+
+export const formatSlot = (slot: number): string => {
+  const hours = Math.floor(slot / 60);
+  const minutes = slot % 60;
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}`;
+};
+
+// console.log(formatSlot(570)); // "09:30"
+
+export const formatDate = (date: Date): string => {
+  // Options pour formater la date
+  const options: Intl.DateTimeFormatOptions = {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  };
+
+  // Formatage
+  return date.toLocaleDateString("fr-FR", options);
+};
