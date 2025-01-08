356b9022bd4540e915d7dea38cd850869091ffbb
dylann97 <d.xavero@hotmail.com>
2025-01-08 14:49:30 +0100

    ♻️ Export bookingDataType in types folder

---
+++ types/bookingData.ts
@@ -0,0 +1,10 @@
+type BookingDataType = {
+  serviceName: string;
+  duration: number;
+  price: number;
+  date: Date | undefined;
+  time: string;
+  clientName: string;
+  clientEmail: string;
+  clientPhone: string;
+};
