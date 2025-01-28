f3bf84842d80c11b30fedeb05a330f3c99b6bbdc
dylann97 <d.xavero@hotmail.com>
2025-01-28 20:43:35 +0100

    ♻️ refactor code

--- components/Navlinks.tsx
+++ components/Navlinks.tsx
@@ -2,24 +2,10 @@
 
 import { navbarLinks } from "@/data";
 import Link from "next/link";
-import Button from "./Button";
-import { PiSignInBold, PiSignOutBold } from "react-icons/pi";
-import { useAuth } from "@/hooks/useAuth";
-import { logout } from "@/actions/authClient";
-import { useRouter } from "next/navigation";
 
 const Navlinks = () => {
-  const router = useRouter();
-  const user = useAuth();
-  console.log(user);
-
-  const handleLogout = async () => {
-    await logout();
-    router.push("/");
-  };
-
   return (
-    <div className="hidden lg:flex items-center gap-4">
+    <div className="hidden lg:flex items-center justify-between gap-8">
       <nav className="flex gap-4 font-medium text-base">
         {navbarLinks.map((link, index) => (
           <Link
@@ -31,21 +17,6 @@ const Navlinks = () => {
           </Link>
         ))}
       </nav>
-
-      {user ? (
-        <Button
-          onClick={handleLogout}
-          color="rose"
-          icon={<PiSignOutBold />}
-          button
-        >
-          Déconnexion
-        </Button>
-      ) : (
-        <Button color="rose" icon={<PiSignInBold />} href="/login">
-          Connexion
-        </Button>
-      )}
     </div>
   );
 };
