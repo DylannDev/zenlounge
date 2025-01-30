af854666c613e6997b8b9e5ad8d833121de1e47b
dylann97 <d.xavero@hotmail.com>
2025-01-30 20:41:15 +0100

    🚧 create forms config file

---
+++ data/ProfileForm.config.ts
@@ -0,0 +1,47 @@
+export const InfosFormFields = [
+  {
+    id: "firstName",
+    name: "firstName",
+    type: "text",
+    placeholder: "Prénom",
+    label: "Prénom",
+    required: true,
+    halfWidth: true,
+  },
+  {
+    id: "lastName",
+    name: "lastName",
+    type: "text",
+    placeholder: "Nom",
+    label: "Nom",
+    required: true,
+    halfWidth: true,
+  },
+  {
+    id: "phone",
+    name: "phone",
+    type: "text",
+    placeholder: "Numéro de téléphone",
+    label: "Téléphone",
+    required: true,
+  },
+];
+
+export const credentialFormFields = [
+  // {
+  //   id: "email",
+  //   name: "email",
+  //   type: "email",
+  //   placeholder: "Adresse email",
+  //   label: "Email",
+  //   required: true,
+  // },
+  {
+    id: "password",
+    name: "password",
+    type: "password",
+    placeholder: "Mot de passe",
+    label: "Mot de passe",
+    required: true,
+  },
+];
