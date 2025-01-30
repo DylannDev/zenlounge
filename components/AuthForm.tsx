6180b87b2314f91ee66f3798dc760299d6bf5e4c
dylann97 <d.xavero@hotmail.com>
2025-01-30 20:37:08 +0100

    ♻️ Refactor code to Keep LoginPage as a server component

---
+++ components/AuthForm.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
+import Loader from "./Loader";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <>
+      <div className="w-full bg-white p-0 pb-6 sm:p-6 rounded-3xl border border-rose-dark">
+        {isLogin ? <LoginForm setLoading={setLoading} /> : <SignupForm />}
+
+        <div className="mt-4 text-center">
+          <p className="text-sm text-blue-light">
+            {isLogin ? "Pas encore de compte ?" : "Déjà un compte ?"}{" "}
+            <button
+              onClick={() => setIsLogin(!isLogin)}
+              className="text-rose-dark font-semibold hover:underline"
+            >
+              {isLogin ? "Inscrivez-vous" : "Connectez-vous"}
+            </button>
+          </p>
+        </div>
+      </div>
+      {loading && <Loader text="Redirection en cours..." fullScreen />}
+    </>
+  );
+};
+
+export default AuthForm;
