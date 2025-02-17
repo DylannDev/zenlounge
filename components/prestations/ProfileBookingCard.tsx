4cb72ebc55214b3ca276e3cc2f0d707cdab5583c
dylann97 <d.xavero@hotmail.com>
2025-02-17 23:19:48 +0100

    ✨ add the dialog modal to confirm cancel of booking

--- components/prestations/ProfileBookingCard.tsx
+++ components/prestations/ProfileBookingCard.tsx
@@ -6,6 +6,16 @@ import { PiCalendarX, PiTimer, PiWallet } from "react-icons/pi";
 import { formatDate, getServiceImage } from "@/lib/utils";
 import SquareButton from "../SquareButton";
 import { cancelBooking } from "@/actions/cancelBooking";
+import { useToast } from "@/hooks/use-toast";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 interface Service {
   id: string;
@@ -29,11 +39,11 @@ const ProfileBookingCard = ({
   onRemove: (id: string) => void;
 }) => {
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { toast } = useToast();
 
   const handleCancel = async () => {
     setLoading(true);
-    setError("");
 
     const response = await cancelBooking({
       userId: service.userId,
@@ -42,75 +52,100 @@ const ProfileBookingCard = ({
     });
 
     if (!response.success) {
-      setError(response.message);
+      toast({
+        title: "Erreur",
+        description: response.message,
+      });
     } else {
-      alert(response.message);
       onRemove(service.id);
+      toast({
+        title: "Réservation annulée",
+        description: "Votre prestation a bien été annulée.",
+      });
     }
 
     setLoading(false);
+    setIsModalOpen(false); // Fermer la modale après l'annulation
   };
 
   return (
-    <li className="flex gap-4 justify-between border border-rose-dark p-4 rounded-2xl bg-white">
-      <div className="flex flex-col gap-6 sm:flex-row justify-between items-end w-full">
-        {/* ✅ Ajout de l'image dynamique */}
-        <div className="flex flex-col sm:flex-row gap-6 w-full h-full">
-          <div className="relative w-full h-full aspect-square max-h-[200px] max-w-full sm:max-h-[130px] sm:max-w-[130px]">
-            <Image
-              fill
-              src={getServiceImage(service.serviceName)}
-              alt={`Image de ${service.serviceName} zen lounge`}
-              className="object-cover rounded-xl"
-            />
-          </div>
-
-          <div className="flex flex-col gap-6 justify-between h-full w-full">
-            <div className="flex flex-col">
-              <h3 className="text-xl font-bold">{service.serviceName}</h3>
-              <p className="text-sm text-blue-light">
-                {formatDate(service.date)} à {service.time}
-              </p>
+    <>
+      <li className="flex gap-4 justify-between border border-rose-dark p-4 rounded-2xl bg-white">
+        <div className="flex flex-col gap-6 sm:flex-row justify-between items-end w-full">
+          {/* ✅ Ajout de l'image dynamique */}
+          <div className="flex flex-col sm:flex-row gap-6 w-full h-full">
+            <div className="relative w-full h-full aspect-square max-h-[200px] max-w-full sm:max-h-[130px] sm:max-w-[130px]">
+              <Image
+                fill
+                src={getServiceImage(service.serviceName)}
+                alt={`Image de ${service.serviceName} zen lounge`}
+                className="object-cover rounded-xl"
+              />
             </div>
 
-            <div className="flex flex-col sm:flex-row gap-2 text-sm text-blue-light">
-              {typeof service.forfaitId === "string" && service.forfaitId && (
-                <div className="flex items-center px-3 py-1 font-semibold bg-rose-dark text-brown-dark rounded-lg w-fit">
-                  Forfait
-                </div>
-              )}
-              {service.price !== 0 && (
+            <div className="flex flex-col gap-6 justify-between h-full w-full">
+              <div className="flex flex-col">
+                <h3 className="text-xl font-bold">{service.serviceName}</h3>
+                <p className="text-sm text-blue-light">
+                  {formatDate(service.date)} à {service.time}
+                </p>
+              </div>
+
+              <div className="flex flex-col sm:flex-row gap-2 text-sm text-blue-light">
+                {typeof service.forfaitId === "string" && service.forfaitId && (
+                  <div className="flex items-center px-3 py-1 font-semibold bg-rose-dark text-brown-dark rounded-lg w-fit">
+                    Forfait
+                  </div>
+                )}
+                {service.price !== 0 && (
+                  <p className="flex items-center gap-1 whitespace-nowrap">
+                    <span className="text-xl text-brown-dark">
+                      <PiWallet />
+                    </span>{" "}
+                    {service.price} €
+                  </p>
+                )}
                 <p className="flex items-center gap-1 whitespace-nowrap">
                   <span className="text-xl text-brown-dark">
-                    <PiWallet />
+                    <PiTimer />
                   </span>{" "}
-                  {service.price} €
+                  {service.duration} min
                 </p>
-              )}
-              <p className="flex items-center gap-1 whitespace-nowrap">
-                <span className="text-xl text-brown-dark">
-                  <PiTimer />
-                </span>{" "}
-                {service.duration} min
-              </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        {!isPastBookings && (
-          <>
-            <SquareButton
-              icon={<PiCalendarX />}
-              onClick={handleCancel}
-              disabled={loading}
-            >
-              {loading ? "Annulation..." : "Annuler"}
-            </SquareButton>
-            {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
-          </>
-        )}
-      </div>
-    </li>
+          {!isPastBookings && (
+            <>
+              <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+                <DialogTrigger asChild>
+                  <SquareButton icon={<PiCalendarX />}>Annuler</SquareButton>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogTitle>Confirmer l'annulation</DialogTitle>
+                  <DialogDescription>
+                    Êtes-vous sûr de vouloir annuler cette réservation ? Cette
+                    action est irréversible.
+                  </DialogDescription>
+                  <DialogFooter className="flex gap-2 sm:gap-1 justify-end">
+                    <SquareButton onClick={() => setIsModalOpen(false)}>
+                      Annuler
+                    </SquareButton>
+                    <SquareButton
+                      variant="destructive"
+                      onClick={handleCancel}
+                      disabled={loading}
+                    >
+                      {loading ? "Annulation..." : "Confirmer"}
+                    </SquareButton>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            </>
+          )}
+        </div>
+      </li>
+    </>
   );
 };
 
