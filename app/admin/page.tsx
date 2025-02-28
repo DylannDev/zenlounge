542a86381c71af1d7c38406845e538518b65a0d6
dylann97 <d.xavero@hotmail.com>
2025-02-28 21:46:35 +0100

    🚧 Protect route with server action getAdminStatus

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,16 @@
+import { getAdminStatus } from "@/actions/getAdminStatus";
 import AdminLoginForm from "@/components/admin/AdminLoginForm";
 import Logo from "@/components/Logo";
+import { redirect } from "next/navigation";
 
-export default function AdminLogin() {
+export default async function AdminLogin() {
+  const isAdmin = await getAdminStatus();
+
+  // ✅ Vérification de l'utilisateur
+  if (isAdmin) {
+    redirect("/admin/bookings"); // ✅ Redirection si admin
+  }
+
   return (
     <section className="max-w-[550px] h-screen w-full mx-auto py-20 flex flex-col justify-center items-center gap-6 relative">
       <div className="absolute top-20">
