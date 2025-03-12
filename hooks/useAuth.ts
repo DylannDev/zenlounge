aeeffac927cbf29616908f1acf285734a573e56d
dylann97 <d.xavero@hotmail.com>
2025-03-12 16:34:02 +0100

    🚧 add router.refresh() to test the button connection changes

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -3,13 +3,18 @@
 import { useState, useEffect } from "react";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/navigation";
 
 export function useAuth() {
   const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
+
+  // console.log("user", user);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      router.refresh();
     });
 
     return () => unsubscribe();
