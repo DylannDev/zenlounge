2e190603f1cbf40f621b50ccad72b83c24d96ca9
dylann97 <d.xavero@hotmail.com>
2025-03-13 11:04:25 +0100

    🚧 Filter active credit and forfaits by date expiration

--- app/(app)/forfaits/[slug]/page.tsx
+++ app/(app)/forfaits/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import { getCurrentUser } from "@/actions/authActions";
 import { getUserBookings } from "@/actions/getUserBookings";
 import ForfaitBooking from "@/components/ForfaitBooking";
-import { convertFirebaseTimestamp } from "@/lib/utils";
 import { redirect } from "next/navigation";
 
 export default async function ForfaitBookingPage() {
@@ -14,11 +13,19 @@ export default async function ForfaitBookingPage() {
   const { forfaits } =
     user && ((await getUserBookings(user.uid, user.email!)) as ForfaitsBooking);
 
+  const now = new Date();
+  now.setHours(0, 0, 0, 0); // Normaliser à minuit
+
   const activeForfaits =
-    forfaits?.map((forfait) => ({
-      ...forfait,
-      createdAt: convertFirebaseTimestamp(forfait.createdAt),
-    })) ?? [];
+    forfaits
+      ?.map((forfait) => ({
+        ...forfait,
+      }))
+      .filter((forfait) => {
+        const expiresAt = new Date(forfait.expiresAt);
+        expiresAt.setHours(0, 0, 0, 0);
+        return expiresAt >= now; // Exclure les forfaits expirés
+      }) ?? [];
 
   return <ForfaitBooking forfaits={activeForfaits} />;
 }
--- app/(app)/reservations/[slug]/page.tsx
+++ app/(app)/reservations/[slug]/page.tsx
@@ -2,14 +2,22 @@ import { getCurrentUser } from "@/actions/authActions";
 import { getUserBookings } from "@/actions/getUserBookings";
 import ServiceBooking from "@/components/ServiceBooking";
 
-export default async function ForfaitBookingPage() {
+export default async function BookingPage() {
   const user = await getCurrentUser();
 
   // 🔹 Récupérer les services, forfaits et crédits
   const userBookings = user && (await getUserBookings(user.uid, user.email!));
 
-  // ✅ Vérification et assignation correcte
-  const credits = userBookings?.credits ?? [];
+  const now = new Date();
+  now.setHours(0, 0, 0, 0); // Normaliser à minuit
 
-  return <ServiceBooking credits={credits} />;
+  // 🔹 Filtrer les crédits actifs (non expirés)
+  const activeCredits =
+    userBookings?.credits?.filter((credit) => {
+      const expiresAt = new Date(credit.expiresAt);
+      expiresAt.setHours(0, 0, 0, 0); // Normaliser la date d'expiration
+      return expiresAt >= now; // Exclure les crédits expirés
+    }) ?? [];
+
+  return <ServiceBooking credits={activeCredits} />;
 }
