b3b3ac85961c5bc7167454b0aca75d3cf316484d
dylann97 <d.xavero@hotmail.com>
2025-03-19 17:42:01 +0100

    🚧 Improve firebase function to check if user is logged in

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -9,16 +9,19 @@ export function useAuth() {
   const [user, setUser] = useState<User | null>(null);
   const router = useRouter();
 
-  // console.log("user", user);
-
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      router.refresh();
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      if (!currentUser) {
+        // ✅ Déconnexion détectée, forcer un refresh propre
+        setUser(null);
+        router.refresh();
+      } else {
+        setUser(currentUser);
+      }
     });
 
     return () => unsubscribe();
-  }, [onAuthStateChanged]);
+  }, [auth]);
 
   return user;
 }
