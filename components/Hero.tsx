351d1843dd3eee5ce335049dfa12a8c46a6fee23
dylann97 <d.xavero@hotmail.com>
2025-03-21 23:27:21 +0100

    🚧 Add behavior for the navbar which hide when scroll down

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import Button from "./Button";
 
 const Hero = () => {
+  // Fonction pour gérer le scroll smooth
+  const handleScroll = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const section = document.getElementById("nosprestations");
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
   return (
     <div className="pb-10">
       <div className="relative w-full aspect-1/2 min-[500px]:aspect-2/3 sm:aspect-3/4 md:aspect-4/3 lg:aspect-video max-h-[calc(100dvh-100px)] lg:max-h-[calc(100dvh-160px)] overflow-hidden">
@@ -30,8 +41,10 @@ const Hero = () => {
             <Button
               color="white"
               responsiveWidth={{ default: "large", md: "normal" }}
+              onClick={handleScroll} // ✅ Déclenchement du scroll smooth
+              button
             >
-              Réserver mon moment
+              Découvrir les prestations
             </Button>
           </div>
         </div>
