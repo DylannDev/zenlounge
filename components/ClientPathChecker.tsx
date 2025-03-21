d35c8d797dba5dd6cfb5850470d59c45ad35c5d2
dylann97 <d.xavero@hotmail.com>
2025-03-21 23:22:24 +0100

    🚧 Add new css class to hide scrollbar only when user is not on route /location

--- components/ClientPathChecker.tsx
+++ components/ClientPathChecker.tsx
@@ -33,6 +33,14 @@ const ClientPathChecker = ({ children }: ClientPathCheckerProps) => {
     };
   }, [lastScrollY]);
 
+  useEffect(() => {
+    if (pathname === "/location") {
+      document.body.classList.remove("no-scroll-x");
+    } else {
+      document.body.classList.add("no-scroll-x");
+    }
+  }, [pathname]);
+
   return (
     <div
       className={`h-[80px] lg:h-[120px] fixed top-0 left-0 right-0 z-50 bg-white/80 backdrop-blur-lg flex items-center transition-transform duration-300 ${
