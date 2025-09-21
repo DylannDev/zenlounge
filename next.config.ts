fa619be047a163f37147c310588f6f1658a03a96
dylann97 <d.xavero@hotmail.com>
2025-09-22 01:11:41 +0200

    🚧 Implement temporary redirects for /location

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      { source: "/location", destination: "/", permanent: false },
+      { source: "/location/:path*", destination: "/", permanent: false },
+    ];
+  },
 };
 
 export default nextConfig;
